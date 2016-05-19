edee5a3609365cdf4c9a17fad72c35d1972376e5
Raja Jawahar <rajajawahar77@gmail.com>
2016-05-19 18:48:19 +0530

    initial commit

---
+++ app/src/main/java/com/silicon/raja/collapsingtoolbarexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.silicon.raja.collapsingtoolbarexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
