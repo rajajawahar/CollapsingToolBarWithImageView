f82d388b0bfbfe296f25c1bfd59cf3a102aa814f
Raja Jawahar <rajajawahar77@gmail.com>
2016-05-20 13:11:40 +0530

    Subtitle added in CollpasingToolbar

---
+++ app/src/main/java/com/silicon/raja/collapsingtoolbarexample/HeaderView.java
@@ -0,0 +1,56 @@
+package com.silicon.raja.collapsingtoolbarexample;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class HeaderView extends LinearLayout {
+
+    TextView title;
+    TextView subTitle;
+
+
+    public HeaderView(Context context) {
+        super(context);
+    }
+
+    public HeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public HeaderView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        title = (TextView) findViewById(R.id.header_view_title);
+        subTitle = (TextView) findViewById(R.id.header_view_sub_title);
+    }
+
+    public void bindTo(String title) {
+        bindTo(title, "");
+    }
+
+    public void bindTo(String title, String subTitle) {
+        hideOrSetText(this.title, title);
+        hideOrSetText(this.subTitle, subTitle);
+    }
+
+    private void hideOrSetText(TextView tv, String text) {
+        if (text == null || text.equals(""))
+            tv.setVisibility(GONE);
+        else
+            tv.setText(text);
+    }
+}
--- app/src/main/java/com/silicon/raja/collapsingtoolbarexample/MainActivity.java
+++ app/src/main/java/com/silicon/raja/collapsingtoolbarexample/MainActivity.java
@@ -1,10 +1,21 @@
 package com.silicon.raja.collapsingtoolbarexample;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private AppBarLayout appBarLayout;
+    private HeaderView toolbarHeaderView;
+    private HeaderView floatHeaderView;
+    private boolean isHideToolbarView = false;
 
 
     @Override
@@ -16,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        getSupportActionBar().setTitle("");
+        appBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        toolbarHeaderView = (HeaderView) findViewById(R.id.toolbar_header_view);
+        floatHeaderView = (HeaderView) findViewById(R.id.float_header_view);
+        floatHeaderView.setGravity(Gravity.CENTER);
+        toolbarHeaderView.bindTo("3 Years", "Android Developer");
+        floatHeaderView.bindTo("Raja Mohamed", "Orgware Technologies");
 
+        appBarLayout.addOnOffsetChangedListener(this);
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        if (percentage == 1f && isHideToolbarView) {
+            toolbarHeaderView.setVisibility(View.VISIBLE);
+            isHideToolbarView = !isHideToolbarView;
+
+        } else if (percentage < 1f && !isHideToolbarView) {
+            toolbarHeaderView.setVisibility(View.GONE);
+            isHideToolbarView = !isHideToolbarView;
+        }
     }
 }
